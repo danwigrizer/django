a1548d6bff13d96b5b31ef7fb8750d0c5ac6ab32
Dan Wigrizer <Wigrizer@gmail.com>
2020-10-04 13:33:02 -0400

    Added registration with login & logout system

--- magazine/users/views.py
+++ magazine/users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 from .form import UserRegisterForm
 from django.contrib.auth.models import User
 import logging
+
 
 def register(request):
 
@@ -13,13 +14,25 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
+            messages.success(request, f'Your account has been created! You can now login')
             logging.debug('Form is valid')
 
-            return redirect('blog-home')
+            return redirect('login')
 
         else:
             logging.error('Form Invalid')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+# def login(request):
+#     if request.method == 'POST':
+#         form = AuthenticationForm(request.POST, next='blog-home')
+#         if form.confirm_login_allowed(form):
+#             return redirect('blog-home')
+#         else:
+#             return redirect('blog-about')
+#     else:
+#         form = AuthenticationForm()
+#     return render(request, 'users/login.html', {form: form})
+
